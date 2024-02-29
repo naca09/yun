08b79763e1185f14cd32f9453f419a5f95c74b43
naca09 <86470016+naca09@users.noreply.github.com>
2024-02-29 17:05:48 +0700

    tien push

--- Project/Project/Project/obj/Debug/net7.0/Project.AssemblyInfo.cs
+++ Project/Project/Project/obj/Debug/net7.0/Project.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7902a57355428be05421a88306d4b431c880e8d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c059519ecf9f6c87cf3f0fc4c2b9effa136cb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
